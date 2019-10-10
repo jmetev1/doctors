23cd5d05b3ebf4a58041acaf55e0e12b1423ec51
Jmetev1 <jmetevier@gmail.com>
2019-10-10 14:59:02 -0700

    add patient schedules

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/phys').then(res => {
+      res.json().then(data => this.setState({ docs: data }))
+    })
+  }
+  docClick(id) {
+    const docs = this.state.docs.slice().map(doc => {
+      doc.selected = doc.id === id ? true : false;
+      return doc
+    })
+    fetch('http://127.0.0.1:8000/patients/' + id).then(res => {
+      res.json().then(data => this.setState({ patients: data, docs }))
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="wrapper">
+          <div className="App">
+            <div className="left">
+              {this.state && this.state.docs && this.state.docs.map(doc => (
+                <div key={doc.id} className={doc.selected ? 'selected' : ''}>
+                  <button onClick={() => this.docClick(doc.id)}>{doc.name}</button>
+                </div>
+              ))}
+            </div>
+            <div className="right">
+              {this.state && this.state.patients ?
+                <Appointments appts={this.state.patients} /> : 'Please click a doctor to see their appointments (and resize the screen to see mobile behavior'}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+const Appointments = ({ appts }) => {
+  return (
+    <table>
+      <tr>
+        <th>Patient</th>
+        <th>Time</th>
+        <th>Kind</th>
+      </tr>
+      {appts.sort((a, b) => a - b).map(appt => (
+        <tr key={appt.name}>
+          <td>
+            {appt.name}
+          </td>
+          <td>
+            {appt.time}:00PM
+            </td>
+          <td>
+            {appt.time % 2 === 0 ? 'New Patient' : "Follow-up"}
+          </td>
+        </tr>
+      ))}
+    </table>
+  )
+}
+
+
 
 export default App;
